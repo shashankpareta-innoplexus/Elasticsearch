caa4f3b17e5211ea0b914228c71d0e8bbebc1461
shashankparetainno <shashankparetainno@users.noreply.github.com>
2016-06-23 09:34:47 +0530

    Update mongoPython3.py

--- mongoPython3.py
+++ mongoPython3.py
@@ -1,42 +1,51 @@
 from pymongo import MongoClient
-import json
 from elasticsearch import helpers
+from bson import json_util
+import json
 import elasticsearch
 import collections
 
-
+# elasticsearch client
 es = elasticsearch.Elasticsearch()
+
+# connection to mongoDB
 connection = MongoClient('localhost',27017)
-db = connection.autocompleteassign.sample_patents
 
-# def convert(data):
-#     if isinstance(data, basestring):
-#         return str(data)
-#     elif isinstance(data, collections.Mapping):
-#         return dict(map(convert, data.iteritems()))
-#     elif isinstance(data, collections.Iterable):
-#         return type(data)(map(convert, data))
-#     else:
-#         return data
+# getting collection
+db = connection.congresses.sample_congresses
 
-
-a = db.find({},{'_id':0})
-i=1
-actions = []
-for i in range(db.count()):
+objectId = []
+j=0
+a = db.find()
+for j in range(db.count()):
     A = a.next()
-    #dummy_json = convert(A)
-    # print A
-    # break
+    x = str(A['_id'])
+    objectId.append(x)
+    # print A['_id']
+    j=j+1
+
+#print 'Fetched object ids'
+b = db.find({},{'_id':0})
+
+i=0
+# this contain list of actions i.e. indexing to be performed on all json objects
+actions = []
+for x in objectId:
+# for i in range(db.count()):
+    B = b.next()
     action = {
-        "_index" : "testbulk4",
-        "_type" : "type1",
-        "_id" : i,
-        "_source": json.dumps(A)
+        "_index" : "patents_standard",
+        "_type" : "sample_patents",
+        "_id" : x,
+        "_source": json.dumps(B, default = json_util.default)
         }
     actions.append(action)
-    i=i+1
+    #break
     print i
+    i=i+1
 
 if(len(actions)>0):
-    helpers.bulk(es, actions, chunk_size=20)
+    helpers.bulk(es, actions, chunk_size=50)
+
+# if(len(actions)>0):
+#     helpers.parallel_bulk(es, actions, thread_count=4, chunk_size=100, max_chunk_bytes=104857600, raise_on_exception=False)
